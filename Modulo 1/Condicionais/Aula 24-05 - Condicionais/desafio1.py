93d5e38b6ca5ae66707332e710218c738398e885
Gabriel dos Santos Silva <gabrieldossantos8469@gmail.com>
2025-10-25 11:12:03 -0300

    Add files via upload

---
+++ Modulo 1/Condicionais/Aula 24-05 - Condicionais/desafio1.py
@@ -0,0 +1,13 @@
+#criar um aplicativo que permite a entrada na balada apenas para maiores de 18 anos
+print('Entrada apenas para maiores de 18 anos')
+
+
+nome = input('qual o seu nome?')
+idade = int('qual a sua idade? ')
+
+if idade <=12:
+    print(f'Cade seus pais {nome}?Volte para sua casa')
+elif idade >=13 and idade <=17:
+    print(f'volte para casa {nome}, voce não tem idade para entrar na festa')
+else:
+    print(f'voce pode entrar na festa {nome}, aproveite!!')
---
+++ Modulo 1/Condicionais/Aula 24-05 - Condicionais/exemplo_if.py
@@ -0,0 +1,17 @@
+#o else sempre sera a ultima condição
+#o elif é o meio termo
+
+idade = 20
+
+if idade <=2:
+    print('Bebê')
+elif idade <= 10: 
+    print('Criança')
+elif idade <= 14:
+    print('Pré adolescente')
+elif idade <= 17: 
+    print('Adolescente')
+elif idade <=60:
+    print('Adulto')
+else:
+    print('Idoso')
